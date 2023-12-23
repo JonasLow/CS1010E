229954c2ecdb9c8030e4edc58da2e57165c5c47b
JonasLow <147266737+JonasLow@users.noreply.github.com>
2023-12-23 22:04:57 +0800

    Question 2: Type of Triangle

---
+++ a1_q2_template.py/a1_q2_template.py
@@ -0,0 +1,11 @@
+def triangle(len1, len2, len3):
+    if (len1 > len2 + len3 or len2 > len1 + len3 or len3 > len1 + len2):
+        return "Not a triangle"
+    elif (len1 == len2 == len3):
+        return "Equilateral"
+    elif ((len1 == len2 and len2 != len3) or (len1 != len2 and len2 == len3) or (len1 == len3 and len1 != len2)):
+        return "Isoceles"
+    else:
+        return "Scalene"
+    
+print(triangle(5,3,3))
