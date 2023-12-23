beb80f8d2abcaeb9b7f6795c5a50bd215a1856e6
JonasLow <147266737+JonasLow@users.noreply.github.com>
2023-12-24 00:49:44 +0800

    Who are the Winners

---
+++ Assignment 1/a1_q6_template.py/a1_q6_template.py
@@ -0,0 +1,24 @@
+def find_winners(factor, must_have, n):
+    winners = 0
+    i = 1
+    while (factor * i) < n:
+        if contain_digit(factor * i, must_have):
+            winners += 1
+        i += 1
+
+    return winners
+
+def contain_digit(number, digit):
+    x = number
+    while x > 0:
+        if (x % 10 == digit):
+            return True
+        else:
+            x = x // 10
+    
+    return False
+
+
+print(find_winners(3, 5, 100))
+print(find_winners(9, 1, 15))
+print(find_winners(7, 7, 200))
