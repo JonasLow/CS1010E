3a1c16f76563b7a97e550b5cc8dae68bf2db56d5
JonasLow <147266737+JonasLow@users.noreply.github.com>
2024-01-20 18:46:18 +0800

    Week 02

---
+++ Tutorials/Week_2/week02.py
@@ -0,0 +1,69 @@
+### Tutorial Week 02
+"""
+1 Arithmetic Evaluations
+"""
+##print(3 * 4 + 5)
+##print(3 + 4 * 5)
+##print(5 ** 3 % 4)
+##print(97 / 4)
+##print(97 // 4)
+ans1 = 17 # Replace with your answer
+ans2 = 23 # Replace with your answer
+ans3 = 1 # Replace with your answer
+ans4 = 24.25 # Replace with your answer
+ans5 = 24 # Replace with your answer 
+
+"""
+3 String Evaluations
+"""
+##print('abc' + "def")
+##print('gala' * 3)
+##print('mu' + 'ha' * 4)
+##print('banana'[3])
+##print('banana'[2:4])
+##print('aaa' > 'aab')
+##print('aaa' > 'Zaa')
+ans1 = "abcdef" # Replace with your answer
+ans2 = "galagalagala" # Replace with your answer
+ans3 = "muhahahaha" # Replace with your answer
+ans4 = "a" # Replace with your answer
+ans5 = "na" # Replace with your answer
+ans6 = False # Replace with your answer
+ans7 = True # Replace with your answer
+
+"""
+4 Relational and Logical Operators
+"""
+##a, b, c = 4, -2, 0
+##print(a > b and b > c)
+##print(a <= c <= b)
+##print(a > b or b > c)
+##print(True or False)
+##print(not not True)
+ans1 = False # Replace with your answer
+ans2 = False # Replace with your answer
+ans3 = True # Replace with your answer
+ans4 = True # Replace with your answer
+ans5 = True # Replace with your answer
+
+
+"""
+8 Turtle Graphics
+"""
+from turtle import *
+fd(100)
+lt(90)
+fd(100)
+lt(90)
+fd(100)
+lt(90)
+fd(100)
+lt(90)
+
+"""
+9 Drawing Square with Turtle
+"""
+def draw_square(d):
+  for i in range(4):
+    fd(d)
+    lt(90)
