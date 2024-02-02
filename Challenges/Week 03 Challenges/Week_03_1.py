b4eff909bce33b8e83ab2ea1d01c5ea35b727475
JonasLow <147266737+JonasLow@users.noreply.github.com>
2024-02-02 16:42:39 +0800

    Staircase Problem

--- Challenges/Week 03 Challenges/Week_03_1.py
+++ Challenges/Week 03 Challenges/Week_03_1.py
@@ -1,15 +1,24 @@
+import sys
+
 def staircase(n):
     if n == 1:
         return 1
     elif n == 2:
         return 2
     else:
-        return staircase(n -1) + staircase(n -2)
+        return staircase(n - 1) + staircase(n - 2)
 
 def main():
-    steps = 10
-    wayz = staircase(steps)
-    print(f"There are {wayz} ways to climb a staircase with {steps} steps!")
+    while True:
+        try:
+            steps = int(input("Please enter an integer: "))
+            if steps <= 0:
+                raise ValueError 
+            wayz = staircase(steps)
+            sys.exit(print(f"There are {wayz} ways to climb a staircase with {steps} steps!"))
+        
+        except ValueError:
+            print("Please provide a real POSITIVE number!")
 
 if __name__ == "__main__":
     main()
